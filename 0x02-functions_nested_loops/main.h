182cad3a8b40e4a0968e1d212f7ff37ba30b480b
Chukwuebuka <shimpsonjc@gmail.com>
2022-09-13 14:06:02 -0700

    fix

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,3 +2,17 @@
 #define ALX_H_INCLUDED
 
 int _putchar(char);
+void print_alphabet(void);
+void print_aalphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+int add(int a, int b);
+void print_to_98(int n);
+void print_time_table(int n);
+void num(int r, int n);
+
+#endif
