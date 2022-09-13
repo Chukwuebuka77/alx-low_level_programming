47ee00c1cc162340eb4eafedb4435d00cbce6155
Chukwuebuka <shimpsonjc@gmail.com>
2022-09-13 15:10:09 -0700

    no 2 fix

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,11 @@
 #include "main.h"
 /**
-* main - check the code
+* print_alphabet_x10 - check the code
 * Return: Always 0.
 */
 void print_alphabet_x10(void)
 {
 	char c;
-	
 	char i;
 
 	for (c = 0;  c <= 9; c++)
