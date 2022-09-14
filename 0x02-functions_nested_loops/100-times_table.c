891c67c6bddbd8b543fea136b52422da1ec337a7
Chukwuebuka <shimpsonjc@gmail.com>
2022-09-14 13:41:40 -0700

    100 times

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -16,30 +16,30 @@ void print_times_table(int n)
 			{
 				res = (i * j);
 				if (j != 0)
-					{
-						_putchar(',');
-						_putchar(' ');
-					}
-					if (res < 10 && j != 0)
-					{
-						_putchar(' ');
-						_putchar(' ');
-						_putchar((res % 10) + '0');
-					}
-					else if (res >= 10 && res < 100)
-					{
-						_putchar(' ');
-						_putchar((res / 10) + '0');
-						_putchar((res % 10) + '0');
-					}
-					else if (res >= 100 && j != 0)
-					{
-						_putchar((res / 100) + '0');
-						_putchar((res / 10) % 10 + '0');
-						_putchar((res % 10) + '0');
-					}
-					else
-						_putchar((res % 10) + '0');
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+				if (res < 10 && j != 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((res % 10) + '0');
+				}
+				else if (res >= 10 && res < 100)
+				{
+					_putchar(' ');
+					_putchar((res / 10) + '0');
+					_putchar((res % 10) + '0');
+				}
+				else if (res >= 100 && j != 0)
+				{
+					_putchar((res / 100) + '0');
+					_putchar((res / 10) % 10 + '0');
+					_putchar((res % 10) + '0');
+				}
+				else
+					_putchar((res % 10) + '0');
 			}
 			_putchar('\n');
 		}
