3a5a5519a93b33f24a8b19043c29bf68db6b622e
Chukwuebuka <shimpsonjc@gmail.com>
2022-09-14 02:02:06 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* _abs - check the code
+* Return: Always 0
+*/
+int _abs(int)
+{
+	int i = abs(i);
+	if (i > 0)
+	{
+		_putchar("\nAbsolute Number = %d", i);
+		_putchar("\nAbsolute Number = %d", i);
+	}
+	return (0);
+}
