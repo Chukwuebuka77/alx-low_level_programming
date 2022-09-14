2e5376ae241ad365c52cd64734717fbe16f067bd
Chukwuebuka <shimpsonjc@gmail.com>
2022-09-14 12:26:13 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,22 +3,61 @@
 * times_table - prints the 9 times table, starting with 0.
 */
 void times_table(void)
+
 {
-	int num, mult, prod;
-	for (num = 0; num <= 9; num++)
-	{
-		_putchar('0');
-		for (mult = 1; mult <= 9; mult++)
-		{
-			_putchar(',');
-			_putchar(' ');
-			prod = num * mult;
-			if (prod <= 9)
-				_putchar(' ');
-			else
-				_putchar((prod / 10) + '0');
-			_putchar((prod % 10) + '0');
-		}
-		_putchar('\n');
-	}
+
+		int i, j, res;
+
+
+
+			for (i = 0; i <= 9; i++)
+
+					{
+
+								for (j = 0; j <= 9; j++)
+
+											{
+
+															res = (i * j);
+
+																		if (j != 0)
+
+																						{
+
+																											_putchar(',');
+
+																															_putchar(' ');
+
+																																		}
+
+																					if (res >= 10)
+
+																									{
+
+																														_putchar((res / 10) + '0');
+
+																																		_putchar((res % 10) + '0');
+
+																																					}
+
+																								else if (res < 10 && j != 0)
+
+																												{
+
+																																	_putchar(' ');
+
+																																					_putchar((res % 10) + '0');
+
+																																								}
+
+																											else
+
+																																_putchar((res % 10) + '0');
+
+																													}
+
+										_putchar('\n');
+
+											}
+
 }
