0cf7a38902e06c2f09b19e3aca6df7740b5ef9d5
Chukwuebuka <shimpsonjc@gmail.com>
2022-09-15 13:27:10 -0700

    test file

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,2 +1,13 @@
-#!/bin/bash
-gcc -o main.c O-main.c
+#include "main.h"
+/**
+* main - Test function for positive or negative
+* Return: 0
+*/
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
