ee8ea451f3a437eca89b87f3650722882d723dce
Chukwuebuka <shimpsonjc@gmail.com>
2022-09-16 06:27:30 -0700

    print numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-* main - check the code for Holberton School students.
+*  print_most_numbers - check the code for Holberton School students.
 * Return: Always 0.
 */
 void print_most_numbers(void)
