c080204ddd7d5c3fba20508f44c3fd112726090d
Chukwuebuka <shimpsonjc@gmail.com>
2022-09-16 08:51:25 -0700

    fizz buzz fixed

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,17 +12,24 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
-			printf("FizzBuzz");
-		else if (i % 3 == 0)
-			printf("Fizz");
-		else if (i % 5 == 0)
-			printf("Buzz");
-		else
-			printf("%d", i);
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			if (i % 3 == 0)
+			{
+				printf("Fizz");
+			}
+			if (i % 5 == 0)
+			{
+				printf("Buzz");
+			}
+		}
+	       	else
+	{
+		printf("%d", i);
 	}
-		if (i != 100)
-			putchar(' ');
-	putchar('\n');
+	if (i != 100)
+		printf(" ");
+	}
+		printf("\n");
 	return (0);
 }
